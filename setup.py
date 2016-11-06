bb6364cadea0180534c7285b7c708d616b037d53
Jan Hajic Jr <hajicj@ufal.mff.cuni.cz>
2016-11-07 00:15:27 +0100

    Added package & setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from __future__ import print_function, unicode_literals
+from setuptools import setup
+
+setup(
+    name='npfl103',
+    version='1.0.0',
+    packages=['npfl103'],
+    url='https://github.com/hajicj/FEL-NLP-IR_2016',
+    license='Apache 2.0',
+    author='Jan Hajic jr.',
+    author_email='hajicj@ufal.mff.cuni.cz',
+    description='Source code for Information Retrieval homeworks (UFAL-taught course for FEL) '
+)
